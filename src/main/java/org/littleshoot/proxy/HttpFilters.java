b543b90a516b96c23101e185399687fa2fe851b0
jv <jv@manz.at>
2016-02-03 15:01:32 +0100

    HttpFilters.java extended with writeBadGatewayResponse, writeBadRequestResponse and writeGatewayTimeoutResponse for customization of responses in error cases

--- src/main/java/org/littleshoot/proxy/HttpFilters.java
+++ src/main/java/org/littleshoot/proxy/HttpFilters.java
@@ -2,6 +2,7 @@ package org.littleshoot.proxy;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.*;
+
 import org.littleshoot.proxy.impl.ProxyUtils;
 
 import java.net.InetSocketAddress;
@@ -70,143 +71,183 @@ import java.net.InetSocketAddress;
  * </p>
  */
 public interface HttpFilters {
-    /**
-     * Filters requests on their way from the client to the proxy. To interrupt processing of this request and return a
-     * response to the client immediately, return an HttpResponse here. Otherwise, return null to continue processing as
-     * usual.
-     * <p/>
-     * <b>Important:</b> When returning a response, you must include a mechanism to allow the client to determine the length
-     * of the message (see RFC 7230, section 3.3.3: https://tools.ietf.org/html/rfc7230#section-3.3.3 ). For messages that
-     * may contain a body, you may do this by setting the Transfer-Encoding to chunked, setting an appropriate
-     * Content-Length, or by adding a "Connection: close" header to the response (which will instruct LittleProxy to close
-     * the connection). If the short-circuit response contains body content, it is recommended that you return a
-     * FullHttpResponse.
-     * 
-     * @param httpObject Client to Proxy HttpRequest (and HttpContent, if chunked)
-     * @return a short-circuit response, or null to continue processing as usual
-     */
-    HttpResponse clientToProxyRequest(HttpObject httpObject);
+	/**
+	 * Filters requests on their way from the client to the proxy. To interrupt
+	 * processing of this request and return a response to the client
+	 * immediately, return an HttpResponse here. Otherwise, return null to
+	 * continue processing as usual.
+	 * <p/>
+	 * <b>Important:</b> When returning a response, you must include a mechanism
+	 * to allow the client to determine the length of the message (see RFC 7230,
+	 * section 3.3.3: https://tools.ietf.org/html/rfc7230#section-3.3.3 ). For
+	 * messages that may contain a body, you may do this by setting the
+	 * Transfer-Encoding to chunked, setting an appropriate Content-Length, or
+	 * by adding a "Connection: close" header to the response (which will
+	 * instruct LittleProxy to close the connection). If the short-circuit
+	 * response contains body content, it is recommended that you return a
+	 * FullHttpResponse.
+	 * 
+	 * @param httpObject
+	 *            Client to Proxy HttpRequest (and HttpContent, if chunked)
+	 * @return a short-circuit response, or null to continue processing as usual
+	 */
+	HttpResponse clientToProxyRequest(HttpObject httpObject);
 
-    /**
-     * Filters requests on their way from the proxy to the server. To interrupt processing of this request and return a
-     * response to the client immediately, return an HttpResponse here. Otherwise, return null to continue processing as
-     * usual.
-     * <p/>
-     * <b>Important:</b> When returning a response, you must include a mechanism to allow the client to determine the length
-     * of the message (see RFC 7230, section 3.3.3: https://tools.ietf.org/html/rfc7230#section-3.3.3 ). For messages that
-     * may contain a body, you may do this by setting the Transfer-Encoding to chunked, setting an appropriate
-     * Content-Length, or by adding a "Connection: close" header to the response. (which will instruct LittleProxy to close
-     * the connection). If the short-circuit response contains body content, it is recommended that you return a
-     * FullHttpResponse.
-     * 
-     * @param httpObject Proxy to Server HttpRequest (and HttpContent, if chunked)
-     * @return a short-circuit response, or null to continue processing as usual
-     */
-    HttpResponse proxyToServerRequest(HttpObject httpObject);
+	/**
+	 * Filters requests on their way from the proxy to the server. To interrupt
+	 * processing of this request and return a response to the client
+	 * immediately, return an HttpResponse here. Otherwise, return null to
+	 * continue processing as usual.
+	 * <p/>
+	 * <b>Important:</b> When returning a response, you must include a mechanism
+	 * to allow the client to determine the length of the message (see RFC 7230,
+	 * section 3.3.3: https://tools.ietf.org/html/rfc7230#section-3.3.3 ). For
+	 * messages that may contain a body, you may do this by setting the
+	 * Transfer-Encoding to chunked, setting an appropriate Content-Length, or
+	 * by adding a "Connection: close" header to the response. (which will
+	 * instruct LittleProxy to close the connection). If the short-circuit
+	 * response contains body content, it is recommended that you return a
+	 * FullHttpResponse.
+	 * 
+	 * @param httpObject
+	 *            Proxy to Server HttpRequest (and HttpContent, if chunked)
+	 * @return a short-circuit response, or null to continue processing as usual
+	 */
+	HttpResponse proxyToServerRequest(HttpObject httpObject);
 
-    /**
-     * Informs filter that proxy to server request is being sent.
-     */
-    void proxyToServerRequestSending();
+	/**
+	 * Informs filter that proxy to server request is being sent.
+	 */
+	void proxyToServerRequestSending();
 
-    /**
-     * Informs filter that the HTTP request, including any content, has been sent.
-     */
-    void proxyToServerRequestSent();
+	/**
+	 * Informs filter that the HTTP request, including any content, has been
+	 * sent.
+	 */
+	void proxyToServerRequestSent();
 
-    /**
-     * Filters responses on their way from the server to the proxy.
-     * 
-     * @param httpObject
-     *            Server to Proxy HttpResponse (and HttpContent, if chunked)
-     * @return the modified (or unmodified) HttpObject. Returning null will
-     *         force a disconnect.
-     */
-    HttpObject serverToProxyResponse(HttpObject httpObject);
+	/**
+	 * Filters responses on their way from the server to the proxy.
+	 * 
+	 * @param httpObject
+	 *            Server to Proxy HttpResponse (and HttpContent, if chunked)
+	 * @return the modified (or unmodified) HttpObject. Returning null will
+	 *         force a disconnect.
+	 */
+	HttpObject serverToProxyResponse(HttpObject httpObject);
 
-    /**
-     * Informs filter that a timeout occurred before the server response was received by the client. The timeout may have
-     * occurred while the client was sending the request, waiting for a response, or after the client started receiving
-     * a response (i.e. if the response from the server "stalls").
-     *
-     * See {@link HttpProxyServerBootstrap#withIdleConnectionTimeout(int)} for information on setting the timeout.
-     */
-    void serverToProxyResponseTimedOut();
+	/**
+	 * Informs filter that a timeout occurred before the server response was
+	 * received by the client. The timeout may have occurred while the client
+	 * was sending the request, waiting for a response, or after the client
+	 * started receiving a response (i.e. if the response from the server
+	 * "stalls").
+	 * 
+	 * See {@link HttpProxyServerBootstrap#withIdleConnectionTimeout(int)} for
+	 * information on setting the timeout.
+	 */
+	void serverToProxyResponseTimedOut();
 
-    /**
-     * Informs filter that server to proxy response is being received.
-     */
-    void serverToProxyResponseReceiving();
+	/**
+	 * Informs filter that server to proxy response is being received.
+	 */
+	void serverToProxyResponseReceiving();
 
-    /**
-     * Informs filter that server to proxy response has been received.
-     */
-    void serverToProxyResponseReceived();
+	/**
+	 * Informs filter that server to proxy response has been received.
+	 */
+	void serverToProxyResponseReceived();
 
-    /**
-     * Filters responses on their way from the proxy to the client.
-     * 
-     * @param httpObject
-     *            Proxy to Client HttpResponse (and HttpContent, if chunked)
-     * @return the modified (or unmodified) HttpObject. Returning null will
-     *         force a disconnect.
-     */
-    HttpObject proxyToClientResponse(HttpObject httpObject);
+	/**
+	 * Filters responses on their way from the proxy to the client.
+	 * 
+	 * @param httpObject
+	 *            Proxy to Client HttpResponse (and HttpContent, if chunked)
+	 * @return the modified (or unmodified) HttpObject. Returning null will
+	 *         force a disconnect.
+	 */
+	HttpObject proxyToClientResponse(HttpObject httpObject);
 
-    /**
-     * Informs filter that proxy to server connection is in queue.
-     */
-    void proxyToServerConnectionQueued();
+	/**
+	 * Informs filter that proxy to server connection is in queue.
+	 */
+	void proxyToServerConnectionQueued();
 
-    /**
-     * Filter DNS resolution from proxy to server.
-     * 
-     * @param resolvingServerHostAndPort
-     *            Server "HOST:PORT"
-     * @return alternative address resolution. Returning null will let normal
-     *         DNS resolution continue.
-     */
-    InetSocketAddress proxyToServerResolutionStarted(
-            String resolvingServerHostAndPort);
+	/**
+	 * Filter DNS resolution from proxy to server.
+	 * 
+	 * @param resolvingServerHostAndPort
+	 *            Server "HOST:PORT"
+	 * @return alternative address resolution. Returning null will let normal
+	 *         DNS resolution continue.
+	 */
+	InetSocketAddress proxyToServerResolutionStarted(String resolvingServerHostAndPort);
 
-    /**
-     * Informs filter that proxy to server DNS resolution failed for the specified host and port.
-     *
-     * @param hostAndPort hostname and port the proxy failed to resolve
-     */
-    void proxyToServerResolutionFailed(String hostAndPort);
+	/**
+	 * Informs filter that proxy to server DNS resolution failed for the
+	 * specified host and port.
+	 * 
+	 * @param hostAndPort
+	 *            hostname and port the proxy failed to resolve
+	 */
+	void proxyToServerResolutionFailed(String hostAndPort);
 
-    /**
-     * Informs filter that proxy to server DNS resolution has happened.
-     * 
-     * @param serverHostAndPort
-     *            Server "HOST:PORT"
-     * @param resolvedRemoteAddress
-     *            Address it was proxyToServerResolutionSucceeded to
-     */
-    void proxyToServerResolutionSucceeded(String serverHostAndPort,
-            InetSocketAddress resolvedRemoteAddress);
+	/**
+	 * Informs filter that proxy to server DNS resolution has happened.
+	 * 
+	 * @param serverHostAndPort
+	 *            Server "HOST:PORT"
+	 * @param resolvedRemoteAddress
+	 *            Address it was proxyToServerResolutionSucceeded to
+	 */
+	void proxyToServerResolutionSucceeded(String serverHostAndPort, InetSocketAddress resolvedRemoteAddress);
 
-    /**
-     * Informs filter that proxy to server connection is initiating.
-     */
-    void proxyToServerConnectionStarted();
+	/**
+	 * Informs filter that proxy to server connection is initiating.
+	 */
+	void proxyToServerConnectionStarted();
 
-    /**
-     * Informs filter that proxy to server ssl handshake is initiating.
-     */
-    void proxyToServerConnectionSSLHandshakeStarted();
+	/**
+	 * Informs filter that proxy to server ssl handshake is initiating.
+	 */
+	void proxyToServerConnectionSSLHandshakeStarted();
 
-    /**
-     * Informs filter that proxy to server connection has failed.
-     */
-    void proxyToServerConnectionFailed();
+	/**
+	 * Informs filter that proxy to server connection has failed.
+	 */
+	void proxyToServerConnectionFailed();
 
-    /**
-     * Informs filter that proxy to server connection has succeeded.
-     *
-     * @param serverCtx the {@link io.netty.channel.ChannelHandlerContext} used to connect to the server
-     */
-    void proxyToServerConnectionSucceeded(ChannelHandlerContext serverCtx);
+	/**
+	 * Informs filter that proxy to server connection has succeeded.
+	 * 
+	 * @param serverCtx
+	 *            the {@link io.netty.channel.ChannelHandlerContext} used to
+	 *            connect to the server
+	 */
+	void proxyToServerConnectionSucceeded(ChannelHandlerContext serverCtx);
+
+	/**
+	 * Defines response in case of a bad gateway situation
+	 * 
+	 * @param httpRequest
+	 *            used for information like url
+	 */
+	DefaultFullHttpResponse writeBadGatewayResponse(HttpRequest httpRequest);
+
+	/**
+	 * Defines response in case of a bad request situation
+	 * 
+	 * @param httpRequest
+	 *            used for information like url
+	 */
+	DefaultFullHttpResponse writeBadRequestResponse(HttpRequest httpRequest);
+
+	/**
+	 * Defines response in case of a gateway timeout situation
+	 * 
+	 * @param httpRequest
+	 *            used for information like url
+	 */
+	DefaultFullHttpResponse writeGatewayTimeoutResponse(HttpRequest httpRequest);
 
 }
--- src/main/java/org/littleshoot/proxy/HttpFiltersAdapter.java
+++ src/main/java/org/littleshoot/proxy/HttpFiltersAdapter.java
@@ -1,101 +1,125 @@
 package org.littleshoot.proxy;
 
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
 
 import java.net.InetSocketAddress;
+
+import org.littleshoot.proxy.impl.ResponseUtils;
 
 /**
  * Convenience base class for implementations of {@link HttpFilters}.
  */
 public class HttpFiltersAdapter implements HttpFilters {
-    protected final HttpRequest originalRequest;
-    protected final ChannelHandlerContext ctx;
+	protected final HttpRequest originalRequest;
+	protected final ChannelHandlerContext ctx;
 
-    public HttpFiltersAdapter(HttpRequest originalRequest,
-            ChannelHandlerContext ctx) {
-        this.originalRequest = originalRequest;
-        this.ctx = ctx;
-    }
+	public HttpFiltersAdapter(HttpRequest originalRequest, ChannelHandlerContext ctx) {
+		this.originalRequest = originalRequest;
+		this.ctx = ctx;
+	}
 
-    public HttpFiltersAdapter(HttpRequest originalRequest) {
-        this(originalRequest, null);
-    }
+	public HttpFiltersAdapter(HttpRequest originalRequest) {
+		this(originalRequest, null);
+	}
 
-    @Override
-    public HttpResponse clientToProxyRequest(HttpObject httpObject) {
-        return null;
-    }
+	@Override
+	public HttpResponse clientToProxyRequest(HttpObject httpObject) {
+		return null;
+	}
 
-    @Override
-    public HttpResponse proxyToServerRequest(HttpObject httpObject) {
-        return null;
-    }
+	@Override
+	public HttpResponse proxyToServerRequest(HttpObject httpObject) {
+		return null;
+	}
 
-    @Override
-    public void proxyToServerRequestSending() {
-    }
+	@Override
+	public void proxyToServerRequestSending() {
+	}
 
-    @Override
-    public void proxyToServerRequestSent() {
-    }
+	@Override
+	public void proxyToServerRequestSent() {
+	}
 
-    @Override
-    public HttpObject serverToProxyResponse(HttpObject httpObject) {
-        return httpObject;
-    }
+	@Override
+	public HttpObject serverToProxyResponse(HttpObject httpObject) {
+		return httpObject;
+	}
 
-    @Override
-    public void serverToProxyResponseTimedOut() {
-    }
+	@Override
+	public void serverToProxyResponseTimedOut() {
+	}
 
-    @Override
-    public void serverToProxyResponseReceiving() {
-    }
+	@Override
+	public void serverToProxyResponseReceiving() {
+	}
 
-    @Override
-    public void serverToProxyResponseReceived() {
-    }
+	@Override
+	public void serverToProxyResponseReceived() {
+	}
 
-    @Override
-    public HttpObject proxyToClientResponse(HttpObject httpObject) {
-        return httpObject;
-    }
+	@Override
+	public HttpObject proxyToClientResponse(HttpObject httpObject) {
+		return httpObject;
+	}
 
-    @Override
-    public void proxyToServerConnectionQueued() {
-    }
+	@Override
+	public void proxyToServerConnectionQueued() {
+	}
 
-    @Override
-    public InetSocketAddress proxyToServerResolutionStarted(
-            String resolvingServerHostAndPort) {
-        return null;
-    }
+	@Override
+	public InetSocketAddress proxyToServerResolutionStarted(String resolvingServerHostAndPort) {
+		return null;
+	}
 
-    @Override
-    public void proxyToServerResolutionFailed(String hostAndPort) {
-    }
+	@Override
+	public void proxyToServerResolutionFailed(String hostAndPort) {
+	}
 
-    @Override
-    public void proxyToServerResolutionSucceeded(String serverHostAndPort,
-            InetSocketAddress resolvedRemoteAddress) {
-    }
+	@Override
+	public void proxyToServerResolutionSucceeded(String serverHostAndPort, InetSocketAddress resolvedRemoteAddress) {
+	}
 
-    @Override
-    public void proxyToServerConnectionStarted() {
-    }
+	@Override
+	public void proxyToServerConnectionStarted() {
+	}
 
-    @Override
-    public void proxyToServerConnectionSSLHandshakeStarted() {
-    }
+	@Override
+	public void proxyToServerConnectionSSLHandshakeStarted() {
+	}
 
-    @Override
-    public void proxyToServerConnectionFailed() {
-    }
+	@Override
+	public void proxyToServerConnectionFailed() {
+	}
 
-    @Override
-    public void proxyToServerConnectionSucceeded(ChannelHandlerContext serverCtx) {
-    }
+	@Override
+	public void proxyToServerConnectionSucceeded(ChannelHandlerContext serverCtx) {
+	}
+
+	@Override
+	public DefaultFullHttpResponse writeBadGatewayResponse(HttpRequest httpRequest) {
+		String body = "Bad Gateway: " + httpRequest.getUri();
+		DefaultFullHttpResponse response = ResponseUtils.responseFor(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_GATEWAY, body);
+		return response;
+	}
+
+	@Override
+	public DefaultFullHttpResponse writeBadRequestResponse(HttpRequest httpRequest) {
+		String body = "Bad Request to URI: " + httpRequest.getUri();
+		DefaultFullHttpResponse response = ResponseUtils.responseFor(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_REQUEST, body);
+		return response;
+	}
+
+	@Override
+	public DefaultFullHttpResponse writeGatewayTimeoutResponse(HttpRequest httpRequest) {
+		String body = "Gateway Timeout to URI: " + httpRequest.getUri();
+		DefaultFullHttpResponse response = ResponseUtils.responseFor(HttpVersion.HTTP_1_1, HttpResponseStatus.GATEWAY_TIMEOUT, body);
+		return response;
+	}
+
 }
